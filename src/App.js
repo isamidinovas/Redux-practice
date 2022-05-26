fb1991a769e875dd8abafa82a6121b0536f9d8b1
isamidinovas <99507186+isamidinovasymbatt@users.noreply.github.com>
2022-05-26 17:04:41 +0600

    Introduction, dealt with useDispatch & useSelector

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
 
 function App() {
+  const dispatch = useDispatch();
+  const cash = useSelector((state) => state.cash);
+  console.log(cash);
+
+  const addCash = (cash) => {
+    dispatch({ type: "ADD_CASH", payload: cash });
+  };
+  const getCash = (cash) => {
+    dispatch({ type: "GET_CASH", payload: cash });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div style={{ fontSize: "23" }}>{cash}</div>
+      <div style={{ display: "flex" }}>
+        <button
+          onClick={() => {
+            addCash(Number(prompt()));
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Попоплнить счет
+        </button>
+        <button
+          onClick={() => {
+            getCash(Number(prompt()));
+          }}
+        >
+          Снять со счета
+        </button>
+      </div>
     </div>
   );
 }
